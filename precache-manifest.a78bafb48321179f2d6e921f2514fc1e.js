000e0ca6ff73c2bcdb9b258228ad338077e71a79
ravindra kumar <rv.kumar1401@gmail.com>
2020-10-17 17:27:14 +0800

    Updates

---
+++ precache-manifest.a78bafb48321179f2d6e921f2514fc1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4883fda831b3f8e8c6c298e20ff16c6",
+    "url": "/product-assessment-demo/index.html"
+  },
+  {
+    "revision": "6495941e7829ae669bee",
+    "url": "/product-assessment-demo/static/css/main.6406d54c.chunk.css"
+  },
+  {
+    "revision": "6f26622cd53ec528ca48",
+    "url": "/product-assessment-demo/static/js/2.bc970ab3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/product-assessment-demo/static/js/2.bc970ab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6495941e7829ae669bee",
+    "url": "/product-assessment-demo/static/js/main.cc662d54.chunk.js"
+  },
+  {
+    "revision": "799900bdcf0f4aa122b7",
+    "url": "/product-assessment-demo/static/js/runtime-main.c7e011da.js"
+  }
+]);
